bd89f5a9ab165720a784ad32a127c162485df2e6
Uladzimir Bartash <bartosh93@yandex.ru>
2021-09-07 00:10:22 +0300

    feat: add new test to program

--- src/index.js
+++ src/index.js
@@ -16,27 +16,58 @@ module.exports = function solveSudoku(matrix) {
     return restest1.filter(el => !(subarr.includes(el)))
   }
 
-  function test3(x, i, j, restest2) {
-    let arr = []
-    
-  }
-  
-
-  for ( let i = 0; i< matrix.length; i++) {
-    for ( let j = 0; j< matrix[0].length; j++) {
-      if(!(matrix[i][j]===0)) {
-        continue
-      }
-      let temp = test1(matrix[i][j], i, j)
-      if (temp.length === 1) {
-        matrix[i][j] = temp[0]
+  function test3(x, i, j, restest2 ) {
+    let arr = [],n,m;
+    if ((i+1)%3===0) {
+       n = i-2
+    } else if ((i%3)===0||i===0) {
+        n = i
       } else {
-        temp = test2(matrix[i][j], i, j, temp)
-        if (temp.length === 1) {
-          matrix[i][j] = temp[0]
-        } else continue
+          n = i-1
+        } 
+    if ((j+1)%3===0) {
+      m = j-2
+    } else if ((j%3)===0||j===0) {
+        m = j
+      } else {
+          m = j-1
+        }
+    for(let c=0;c<3;n++,c++) {
+      for(let d=0;d<3;m++,d++) {
+        if(matrix[n][m]) arr.push(matrix[n][m])
       }
     }
+    return restest2.filter(el => !(arr.includes(el)))
   }
+  let isChanged = true
+  function walk(matrix) {
+    
+    for ( let i = 0; i< matrix.length; i++) {
+      for ( let j = 0; j< matrix[0].length; j++) {
+        if(!(matrix[i][j]===0)) {
+          continue
+        }
+        let temp = test1(matrix[i][j], i, j)
+        if (temp.length === 1) {
+          matrix[i][j] = temp[0]
+          isChanged = false
+        } else {
+          temp = test2(matrix[i][j], i, j, temp)
+          if (temp.length === 1) {
+            matrix[i][j] = temp[0]
+            isChanged = false
+          } else {
+            temp = test3(matrix[i][j], i, j, temp)
+            if (temp.length === 1) {
+              matrix[i][j] = temp[0]
+              isChanged = false
+            } else isChanged = true
+          }
+        }
+      }
+    }
+    if(!isChanged) walk(matrix)
+  }
+  walk(matrix)
   return matrix
 }
